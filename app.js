69a3d950762ad526ac377dd91882894a235d5f08
Óscar Vázquez <oscarvb94@gmail.com>
2016-01-27 13:33:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var mongoose = require('mongoose');
 var methodOverride = require('method-override');
 
 // database connect
-mongoose.connect('mongodb://localhost:3000/tracks', function(err, res) {
+mongoose.connect('mongodb://s1/tracks', function(err, res) {
   if (err) {
     console.log('ERROR: connecting to Database. ' + err);
   } else {
