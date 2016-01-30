b7388ac928fd2dd3f31f61c7607050f39b432540
Óscar Vázquez <oscarvb94@gmail.com>
2016-01-30 18:19:55 +0100

    Update track.js

--- models/track.js
+++ models/track.js
@@ -5,8 +5,8 @@ Modelo de datos de canciones (track)
 track_id: {
 	name: nombre de la canción,
 	url: url del fichero de audio
-	namecover: nombre de la carátula
-	urlcover: url de la carátula
+	name_cover: nombre de la carátula
+	url_cover: url de la carátula
 } 
 
 */
@@ -15,10 +15,10 @@ var mongoose = require('mongoose'),
     Schema   = mongoose.Schema;
 
 var trackSchema = new Schema({  
-	name:    { type: String },
-	url:     { type: String },
-	imgname: { type: String },
-	urlImg:  { type: String }
+	name:       { type: String },
+	url:        { type: String },
+	name_cover: { type: String },
+	url_cover:  { type: String }
 });
 
 module.exports = mongoose.model('Track', trackSchema);  
